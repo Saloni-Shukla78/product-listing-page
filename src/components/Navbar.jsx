75d7f807e9100035dd6d10daffdfdf311498f4f0
Saloni Shukla <salonishukla875@gmail.com>
2025-09-18 12:11:47 +0530

    Add cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ShoppingCart } from "lucide-react";
 
 const Navbar = () => {
   return (
@@ -7,7 +8,7 @@ const Navbar = () => {
         <div className='flex items-center'>
             <img src='/logo.png' alt='logo' className='w-32'></img>
         </div>
-        <ul className='flex space-x-10'>
+        <ul className='flex space-x-16'>
             <li><a href='#' className=''>HOME</a></li>
             <li><a href='#' className=''>BAG</a></li>
             <li><a href='#' className=''>SNEAKERS</a>
@@ -15,9 +16,8 @@ const Navbar = () => {
             <li><a href='#' className=''>BELT</a></li>
             <li><a href='#' className=''>CONTACT</a></li>
         </ul>
-        <ul className='flex space-x-3'>
-            <li> 
-            </li>
+        <ul className='flex space-x-10'>
+            <li><ShoppingCart /></li>
             <li>items</li>
             <li>$0.00</li>
         </ul>
