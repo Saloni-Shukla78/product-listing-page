ea4d7c3a4b45eda7e4d8f85a314e886ed981fb5d
Saloni Shukla <salonishukla875@gmail.com>
2025-09-21 22:22:51 +0530

    Make responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { FaFacebookF ,FaTwitter } from "react-icons/fa";
-import { Facebook ,Twitter } from "lucide-react";
+
 
 const Footer = () => {
   return (
     <>
-    <footer className='bg-[#b3d9f8] text-gray-800 px-20 py-12'>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-x-52'>
+    <footer className='bg-[#b3d9f8] text-gray-800 px-7 md:px-20 py-8 md:py-12 sm:px-10 sm:py-12'>
+        <div className='grid  grid-cols-1 md:grid-cols-3  gap-10 md-gap-x-38 lg:gap-x-52'>
             <div className=''>
                 <div className='w-30 mb-3'>
                     <img src='/logo.png' alt='logo'/>
@@ -14,7 +14,7 @@ const Footer = () => {
                 <p className='text-sm leading-relaxed'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.Since the 1500s, when an unknown printer.</p>
             </div>
             <div>
-                <h3 className='text-lg font-semibold mb-3'>Follow Us</h3>
+                <h3 className='font-sans text-[18px] mb-3'>Follow Us</h3>
                 <p className='text-sm mb-3'>Since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
                 <div className='flex space-x-12 text-lg'>
                     <FaFacebookF className='text-blue-800' />
@@ -22,14 +22,14 @@ const Footer = () => {
                 </div>
             </div>
             <div>
-                <h3 className='text-lg font-semibold mb-3'>Contact Us</h3>
-                <p className='text-sm'>E-Comm , 4578 <br/> Marmora Road, <br/>Glasgow D04 89GR</p>
+                <h3 className='font-sans text-[18px] mb-3'>Contact Us</h3>
+                <p className='font-sans text-[14px] text-sm'>E-Comm , 4578 <br/> Marmora Road, <br/>Glasgow D04 89GR</p>
             </div>
         </div>
-        <div className=' mx-auto grid grid-cols-2 md:grid-cols-4 gap-8 mt-12 text-sm'>
+        <div className=' mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 mt-12 text-sm'>
             <div>
-                <h4 className='font-semibold mb-2'>Information</h4>
-                <ul className='space-y-1'>
+                <h4 className='font-sans text-[18px] mb-2'>Information</h4>
+                <ul className='font-sans text-[14px] space-y-1'>
                     <li>About Us</li>
                     <li>Information</li>
                     <li>Privacy Policy</li>
@@ -37,8 +37,17 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h4 className='font-semibold mb-2'>Service</h4>
-                <ul className='space-y-1'>
+                <h4 className='font-sans text-[18px] mb-2'>Service</h4>
+                <ul className='font-sans text-[14px] space-y-1'>
+                    <li >About Us</li>
+                    <li>Information</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms & Conitions</li>
+                </ul>
+            </div>
+            <div>
+                <h4 className='font-sans text-[18px] mb-2'>My Account</h4>
+                <ul className='font-sans text-[14px] space-y-1'>
                     <li>About Us</li>
                     <li>Information</li>
                     <li>Privacy Policy</li>
@@ -46,17 +55,8 @@ const Footer = () => {
                 </ul>
             </div>
             <div>
-                <h4 className='font-semibold mb-2'>My Account</h4>
-                <ul className='space-y-1'>
-                    <li>About Us</li>
-                    <li>Information</li>
-                    <li>Privacy Policy</li>
-                    <li>Terms & Conitions</li>
-                </ul>
-            </div>
-            <div>
-                <h4 className='font-semibold mb-2'>Our Offer</h4>
-                <ul className='space-y-1'>
+                <h4 className='font-sans text-[18px] mb-2'>Our Offer</h4>
+                <ul className='font-sans text-[14px] space-y-1'>
                     <li>About Us</li>
                     <li>Information</li>
                     <li>Privacy Policy</li>
